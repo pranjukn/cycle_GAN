0cdbe212e1bfdbb42be0fce713b6ca8da1dc9604
pranjukn <82752808+pranjukn@users.noreply.github.com>
2021-07-22 10:53:04 +0300

    Update train_options.py

--- cycle_GAN_git/options/train_options.py
+++ cycle_GAN_git/options/train_options.py
@@ -1,4 +1,4 @@
-from base_options import BaseOptions
+from cycle_GAN.cycle_GAN_git.options.base_options import BaseOptions
 
 class TrainOptions(BaseOptions):
     """This class includes training options.
